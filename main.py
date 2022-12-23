462c36bab1966b0d40dfab9a4a6f38def6bb2b24
hengshu1 <hengshu1@ualberta.ca>
2022-12-22 18:59:47 -0700

    combine models into target net space and evaluate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse, sys, time
 import numpy as np
+import tensorflow as tf
 import gym
 
 from agent import Agent
@@ -8,12 +9,25 @@ from agent import Agent
 This code is developed based on https://github.com/DanielPalaio/MountainCar-v0_DeepRL
 '''
 
+def combine_models(net1, net2, alpha=0.5):
+    '''
+    combine the two nets into a single one according to alpha
+    '''
+    w1 = np.array(net1.get_weights())
+    w2 = np.array(net2.get_weights())
+    print('w1.shape=', w1.shape)
+    print('w2.shape=', w2.shape)
+    sys.exit(1)
+    net1.set_weights(alpha*w1 + (1-alpha)*w2)
+    return net1
+
+
 if __name__ == '__main__':
     env = gym.make("MountainCar-v0")
     spec = gym.spec("MountainCar-v0")
 
     parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-    parser.add_argument('--mode', default='testn', type=str, help='train or test1 or testn')
+    parser.add_argument('--mode', default='tn_search', type=str, help='train, test1, testn, or tn_search')#todo: tn_search is for search better models in the target net space
 
     args = parser.parse_args()
 
@@ -40,6 +54,19 @@ if __name__ == '__main__':
             print('testing one model takes:', time.time() -  t0)
         np.save('avg_score.npy', avg_score)
         np.save('std_score.npy', std_score)
+    elif args.mode == 'tn_search':
+
+        file1 = 'saved_networks/dqn_model_' + str(1400)
+        file2 = 'saved_networks/dqn_model_' + str(1499)
+
+        net1 = tf.keras.models.load_model(file1)
+        net2 = tf.keras.models.load_model(file2)
+        model = combine_models(net1, net2, alpha=0.5)
+
+        dqn_agent.q_net = model
+        avg_score, std_score, _ = dqn_agent.test(env, num_episodes=30, file_type='tf', file=file, graph=True)
+
+
     else:
         print('unknown mode. exit')
         sys.exit(1)
