a5d2e212c282a1c39150373e0c9dd796578e13ae
hengshuai yao <hengshu1@ualberta.ca>
2023-01-05 14:46:56 -0700

    add testing models

--- dqn_model.py
+++ dqn_model.py
@@ -7,6 +7,6 @@ def DeepQNetwork(lr, num_actions, input_dims, fc1, fc2):
     q_net.add(Dense(fc1, input_dim=input_dims, activation='relu'))
     q_net.add(Dense(fc2, activation='relu'))
     q_net.add(Dense(num_actions, activation=None))
-    q_net.compile(optimizer=Adam(learning_rate=lr), loss='mse')
+    q_net.compile(optimizer=Adam(learning_rate=lr), loss='mse')#Keras use the sum over batch by Auto: https://github.com/keras-team/keras/blob/v2.11.0/keras/losses.py#L318
     return q_net
 
